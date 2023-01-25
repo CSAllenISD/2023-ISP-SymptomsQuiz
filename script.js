87fca1dd8a4dcfe21347d36bf107e8647babfdbf
tejasvig12 <tejasvi.gutta@student.allenisd.org>
2023-01-25 09:50:56 -0600

    Did the javascript for the buttons +showing result

---
+++ script.js
@@ -0,0 +1,21 @@
+//Initial Scores
+
+var coldScore = 0;
+var feverScore = 0;
+
+
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+
+
+
+var showResultButton = document.getElementById("showResult");
+var restartButton = document.getElementById("restart");
+var result = document.getElementById("result");
+
+//show result button
+
+showResultButton.addEventListener("click", showResult);
+
+
+
